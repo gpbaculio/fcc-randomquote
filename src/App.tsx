c241299d94f71096f0728939b07acf09657b18fb
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-06 14:59:41 +0800

    random quote ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,121 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { getRandomColor } from './utils';
+import axios from 'axios';
+import { css } from '@emotion/core';
+import { FaTwitter, FaTumblr } from 'react-icons/fa';
+import { ClipLoader } from 'react-spinners';
+
 import './App.css';
 
-class App extends Component {
+const override: any = css`
+  display: block;
+  margin: 0 auto;
+`;
+
+interface AppProps {}
+interface AppState {
+  color: string;
+  quote: null | string;
+  author: null | string;
+  loading: boolean;
+}
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      color: getRandomColor(),
+      quote: null,
+      author: null,
+      loading: false
+    };
+  }
+  componentDidMount() {
+    this.fetchQuote();
+  }
+  fetchQuote = () => {
+    this.setState({ loading: true });
+    axios
+      .get('https://thesimpsonsquoteapi.glitch.me/quotes?count=1')
+      .then(({ data }) => {
+        const { quote, character } = data[0];
+        this.setState({
+          quote,
+          author: character,
+          loading: false,
+          color: getRandomColor()
+        });
+      });
+  };
+
   render() {
+    const { author, color, quote, loading } = this.state;
+    const text = `${quote} - ${author}`;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        style={{
+          backgroundColor: color
+        }}
+        className='App d-flex justify-content-center align-items-center'>
+        <div
+          style={{
+            color
+          }}
+          id='quote-box'>
+          <div className='p-5 d-flex flex-column'>
+            {!!loading ? (
+              <ClipLoader
+                css={override}
+                sizeUnit={'px'}
+                size={60}
+                color={color}
+                loading={loading}
+              />
+            ) : (
+              <React.Fragment>
+                <div id='text'>{`"${quote}"`}</div>
+                <div className='text-right mt-3' id='author'>
+                  {`- ${author}`}
+                </div>
+              </React.Fragment>
+            )}
+            <div className='mt-3 d-flex justify-content-between'>
+              <div className='media-buttons'>
+                <a
+                  id='tweet-quote'
+                  target='_blank'
+                  href={`https://twitter.com/intent/tweet?hashtags=quotes&related=gpbaculio&text=${text}`}
+                  style={{
+                    backgroundColor: color,
+                    color: '#f5f5f5'
+                  }}
+                  className={`btn btn-sm ${!!loading && 'disabled'}`}>
+                  <FaTwitter />
+                </a>
+                <a
+                  id='tumblr-quote'
+                  target='_blank'
+                  href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,gpbaculio&caption=${author}&content=${quote}&&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'`}
+                  style={{
+                    backgroundColor: color,
+                    color: '#f5f5f5'
+                  }}
+                  className={`ml-2 btn btn-sm ${!!loading && 'disabled'}`}>
+                  <FaTumblr />
+                </a>
+              </div>
+              <button
+                id='new-quote'
+                type='button'
+                style={{ backgroundColor: color, color: '#f5f5f5' }}
+                className='btn btn-sm'
+                disabled={loading}
+                onClick={() => this.fetchQuote()}>
+                New Quote
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
